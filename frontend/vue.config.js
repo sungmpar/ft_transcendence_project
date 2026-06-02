f4709f7f5adf6a9c9821b1b53a31f4d087fee478
sungmin <ssafysm@gmail.com>
2026-06-03 04:31:43 +0900

    fix_vue_config

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,4 +2,11 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
+
+  devServer: {
+    allowedHosts: [
+      'transcendence.koreacentral.cloudapp.azure.com',
+      '4.218.8.163',
+    ],
+  },
 })
